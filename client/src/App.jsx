a8af17416c36d3c8ab84ec8f1862f19fd4cc660b
Kenzo Nagari <kenzonagari@gmail.com>
2022-12-08 16:26:59 +0800

    base file - client side

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+
+  return (
+    <div className="App" style={{minWidth: "100vw", minHeight: "100vh"}}>
+
+    </div>
+  )
+}
+
+export default App
